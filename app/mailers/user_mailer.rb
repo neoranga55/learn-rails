77abe805d7892a08aaf1478729f47be8ab69f6c1
Sebas LG <neoranga55@gmail.com>
2013-12-01 16:51:29 +0000

    setup custom Zoho mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
   
   def contact_email(contact)
     @contact = contact
-    mail(to: ENV["OWNER_EMAIL"], from: @contact.email, reply_to: @contact.email, :subject => "Website Contact")
+    #mail(to: ENV["OWNER_EMAIL"], from: @contact.email, reply_to: @contact.email, :subject => "Website Contact")
+    mail(to: ENV["OWNER_EMAIL"], from: ENV["MAIL_USERNAME"], reply_to: @contact.email, :subject => "Website Contact")
   end
 end
